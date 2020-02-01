9231dcab4f60c54693ef68801d17dc2fdfab50bb
david-vanderhaar <d.vanderhaarhunter@gmail.com>
2020-01-31 23:55:03 -0500

    WIP container slots

--- src/nystrum/entites.js
+++ src/nystrum/entites.js
@@ -288,18 +288,33 @@ const Containing = superclass => class extends superclass {
     this.container = container;
   }
 
+  createSlot (item) {
+    let slot = {
+      itemType: item.name,
+      items: [item],
+    }
+    this.container.push(slot)
+  }
+
   contains (itemType) {
     let container = this.container;
-    let items = container.filter((item) => item.name === itemType);
-    return items.length > 0 ? items[0] : false;
+    let slots = container.filter((slot) => slot.itemType === itemType);
+    return slots.length > 0 ? slots[0] : false;
   }
 
   addToContainer (item) {
-    this.container.push(item);
+    const index = this.container.findIndex((slot) => slot.itemType === item.name);
+    if (index >= 0) {
+      this.container[index].items.push(item);
+    } else {
+      this.createSlot(item);
+    }
   }
   
   removeFromContainer (item) {
-    this.container = this.container.filter((it) => it.id !== item.id);
+    this.container.forEach((slot) => {
+      slot = slot.items.filter((it) => it.id !== item.id);
+    });
   }
 }
 
